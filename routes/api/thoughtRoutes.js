3f33e358d890c8cee25598913a2b4aa372180b54
tiiingaling <93777306+tiiingaling@users.noreply.github.com>
2023-02-27 22:10:52 +0000

    add API routes, connection file, eg controllers

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+// Import your Thought schema here
+const Thought = require('../models/Thought');
+
+// GET all thoughts
+router.get('/thoughts', async (req, res) => {
+  try {
+    const thoughts = await Thought.find();
+    res.json(thoughts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// GET a single thought by its _id and populated thought and friend data
+router.get('/thoughts/:id', async (req, res) => {
+  try {
+    const thought = await Thought.findById(req.params.id)
+      .populate('reactions')
+      .populate('thoughts');
+    if (!thought) {
+      return res.status(404).json({ message: 'Thought not found' });
+    }
+    res.json(thought);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// POST a new thought
+router.post('/thoughts', async (req, res) => {
+  const thought = new Thought({
+    thoughtText: req.body.thoughtText,
+    username: req.body.username,
+    userId: req.body.userId
+  });
+  try {
+    const newThought = await thought.save();
+    res.status(201).json(newThought);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+// Import your User schema here
+const User = require('../models/User');
+
+// GET all users
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// GET a single user by its _id and populated thought and friend data
+router.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+      .populate('thoughts')
+      .populate('friends');
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// POST a new user
+router.post('/users', async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    age: req.body.age
+  });
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+module.exports = router;
